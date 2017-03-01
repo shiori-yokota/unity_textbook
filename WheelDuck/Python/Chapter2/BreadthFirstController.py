8797749745ffd3a4bcf1d01734f60dba17c8f7e2
yokota <e1252225@gmail.com>
2017-03-01 19:57:54 +0900

    Add : BreadthFirstController.py

---
+++ WheelDuck/Python/Chapter2/BreadthFirstController.py
@@ -0,0 +1,27 @@
+﻿import clr
+clr.AddReferenceByPartialName('UnityEngine')
+import UnityEngine
+import sys
+sys.path.append(UnityEngine.Application.dataPath + '/../Python/Lib')
+
+list = ["S3", "S4", "S1"]
+
+#while OPENLIST:
+UnityEngine.Debug.Log('OPENLIST : ' + str(OPENLIST))
+UnityEngine.Debug.Log('CLOSEDLIST : ' + str(CLOSEDLIST))
+
+queue = OPENLIST
+
+while queue:
+	label = queue.pop(0)
+	if label == GOAL:
+		CLOSEDLIST.append(label)
+		# 解が発見されて終了
+		UnityEngine.Debug.Log('Finish Python code!!')
+
+	if label not in CLOSEDLIST:
+		CLOSEDLIST.append(label)
+		queue += list
+
+UnityEngine.Debug.Log('OPENLIST : ' + str(OPENLIST))
+UnityEngine.Debug.Log('CLOSEDLIST : ' + str(CLOSEDLIST))

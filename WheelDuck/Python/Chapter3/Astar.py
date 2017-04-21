6d3f7d75b14d4b395abb3d507b516059da0dd084
shiori-yokota <e1252225@gmail.com>
2017-04-21 18:01:47 +0900

    Modify : process

--- WheelDuck/Python/Chapter3/Astar.py
+++ WheelDuck/Python/Chapter3/Astar.py
@@ -4,35 +4,36 @@ import UnityEngine
 import sys
 sys.path.append(UnityEngine.Application.dataPath + '/../Python/Lib')
 
-list = [] 
-list.append(["S3", 8])
-list.append(["S4", 5])
-list.append(["S6", 3])
-list.append(["G", 0])
+########## Example ###########
+
+Property_list = []
+Property_list.append(["S1", 8])
+Property_list.append(["S2", 5])
+Property_list.append(["S3", 8])
+Property_list.append(["S4", 5])
+Property_list.append(["S5", 6])
+Property_list.append(["S6", 3])
+Property_list.append(["S7", 5])
+Property_list.append(["S8", 4])
+Property_list.append(["S9", 5])
+Property_list.append(["S10", 3])
+Property_list.append(["G", 0])
 
 UnityEngine.Debug.Log('OPENLIST : ' + str(OPENLIST))
 UnityEngine.Debug.Log('CLOSEDLIST : ' + str(CLOSEDLIST))
 
-stack = OPENLIST
 
-#while stack:
-#	label = stack.pop(0)
-#	if label == GOAL:
-#		CLOSEDLIST.append(label)
-#		# 解が発見されて終了
-#		UnityEngine.Debug.Log('Finish python code!!')
 
-#	if label not in CLOSEDLIST:
-#		CLOSEDLIST.append(label)
-#		stack = list + OPENLIST
+##############################
 
-# CLOSEDLISTに状態名を入れてください
-flat_list = []
-for e in list:
-	flat_list.extend(e)
-list_str = flat_list[0::2]
+# コスト付きの状態集合から状態集合だけを取り出す処理
+# flat_list = []
+# for e in Property_list:
+#	flat_list.extend(e)
+# list_str = flat_list[0::2]
+# CLOSEDLIST = OPENLIST + list_str
 
-CLOSEDLIST = OPENLIST + list_str
+CLOSEDLIST = ["S", "S3", "S4"]
 
 UnityEngine.Debug.Log('OPENLIST : ' + str(OPENLIST))
 UnityEngine.Debug.Log('CLOSEDLIST : ' + str(CLOSEDLIST))

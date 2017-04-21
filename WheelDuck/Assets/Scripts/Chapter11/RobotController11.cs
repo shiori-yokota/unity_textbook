97c65ebb43163a39d8009730d1b468adcf1f1c1f
shiori-yokota <e1252225@gmail.com>
2017-04-21 13:58:27 +0900

    Add : Chapter11

---
+++ WheelDuck/Assets/Scripts/Chapter11/RobotController11.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System;
+
+public class RobotController11 : MonoBehaviour
+{
+    GameObject robot;
+    
+    bool execute = true;
+
+    float speed = 0.03f;
+
+    private string datetimeStr;
+
+    void Start()
+    {
+        robot = GameObject.Find("RobotPy");
+
+    }
+
+    private void Update()
+    {
+        datetimeStr = System.DateTime.Now.Year.ToString() + System.DateTime.Now.Month.ToString().PadLeft(2, '0')
+                        + System.DateTime.Now.Day.ToString().PadLeft(2, '0') + System.DateTime.Now.Hour.ToString().PadLeft(2, '0')
+                        + System.DateTime.Now.Minute.ToString().PadLeft(2, '0') + System.DateTime.Now.Second.ToString().PadLeft(2, '0');
+        
+        if (execute)
+        {
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                UnityEngine.Debug.Log(" Start ");
+                execute = false;
+
+            }
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            robot.transform.position += robot.transform.forward * speed;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            robot.transform.position += -robot.transform.forward * speed;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            robot.transform.Rotate(new Vector3(0, 1f, 0));
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            robot.transform.Rotate(new Vector3(0, -1f, 0));
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Debug.Log("*** Capture ***");
+            Application.CaptureScreenshot(Application.dataPath + "/ScreenShot/" + datetimeStr + ".jpg");
+        }
+            
+    }
+    
+}
---
+++ WheelDuck/Python/Chapter11/Moderator.py
@@ -0,0 +1,8 @@
+﻿import clr
+clr.AddReferenceByPartialName('UnityEngine')
+import UnityEngine
+import sys
+sys.path.append(UnityEngine.Application.dataPath + '/../Python/Lib')
+
+# 迷路のサイズ
+SIZE = 5
